acc890a7e77219c6c5d42b24fe88b2955cfd39f7
Brandon Sandoval <105913779+brand310@users.noreply.github.com>
2022-09-06 20:44:56 -0700

    minor px changes aswell as character adjustment

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//character and block variables
+// Character and block variables
 var character = document.getElementById("character");
 var block = document.getElementById("block")
 
@@ -12,11 +12,11 @@ function jump() {
     },500) 
 }
 
-// this section is a check to see if or when the character var comes in contact with the block var 
+// This section is a check to see if or when the character var comes in contact with the block var 
 var check = setInterval(function(){
     var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if (blockLeft< 200 && blockLeft>0 && characterTop>= 450) {
+    if (blockLeft < 180 && blockLeft > 0 && characterTop >= 200) {
         block.style.animation = "none"
         block.style.animation = "display"
         alert("You Lose. Try Again! Press the reset game button.")
@@ -25,17 +25,14 @@ var check = setInterval(function(){
 });
 
 
-
-
-// Audio 
+// Audio function to decrease volume of background music
 var audio = 
-
 document.getElementById("gameBGM");
 audio.volume = 0.1;
 
 
 
-
+// Score counter
 var counts=setInterval(updated);
 		var upto=0;
 		function updated(){
